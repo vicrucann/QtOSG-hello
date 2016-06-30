e4ee487f91db1ed19973e3f36f79dc5f878483c7
Victoria Rudakova <vicrucann@gmail.com>
2016-06-30 10:56:13 -0400

    desciption added

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
  *
  *       Filename:  main.cpp
  *
- *    Description:  
+ *    Description: Minimalistic project example that uses both Qt and OpenSceneGraph libraries. 
  *
  *        Version:  1.0
  *        Created:  30-Jun-16 10:23:06 AM
